17925461959b503d393e7894691712cd8b31efda
Adnan Sweeney <adnansweeney@gmail.com>
2020-09-25 13:12:54 -0400

    Initial Desktop Styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,34 @@
 import React from "react"
+import github from "../assets/githubIcon.svg"
+import gmail from "../assets/gmailIcon.svg"
+import linkedin from "../assets/linkedinIcon.svg"
 
 export default function Home() {
-  return <div>Adnan Sweeney</div>
+  return (
+    <div class="container">
+     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet" />
+
+
+      <header class="white-text header"> 
+        <p className="header-link"> Resume </p>
+        <p className="header-link"> Projects </p>
+        <p className="header-link"> Contact </p> </header>
+
+      <div>
+        <p class="title white-text"> Adnan Sweeney </p>
+      </div>
+
+      <div>
+        <p class="description white-text">
+          I like making responsive web tools like manaculator.surge.sh
+        </p>
+      </div>
+
+      <div class="flex-icons-container">
+        <img src={linkedin} className="contact-logo" alt="Linkedin Icon" />
+        <img src={github} className="contact-logo" alt="Github Icon" />
+        <img src={gmail} className="contact-logo" alt="Gmail Icon" />
+      </div>
+    </div>
+  )
 }
